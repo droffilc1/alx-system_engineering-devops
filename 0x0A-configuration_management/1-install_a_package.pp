d7d6ec29e1a6a606eea2ad77c0038f2042150992
Clifford Mapesa <97587370+droffilc1@users.noreply.github.com>
2024-01-19 19:23:02 +0300

    fix: requirement fail

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,13 @@ package {'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'Python':
+  ensure   => '3.8.10',
+  provider => 'pip3',
+}
