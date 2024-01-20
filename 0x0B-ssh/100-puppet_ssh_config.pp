c1bb25919cc67d4f61d1b42ec76d7c2c770d8544
Clifford Mapesa <97587370+droffilc1@users.noreply.github.com>
2024-01-20 10:58:25 +0300

    fix: typo

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,15 @@
-# using Puppet to make changes to a configuration file
+# Using Puppet to make changes to a configuration file
 include stdlib
 
 file_line { 'Turn off passwd auth':
-    ensure   => present,
-    path     => '/etc/ssh/ssh_config',
-    line     => '   PasswordAuthentication no'
-    replace  => true,
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => 'PasswordAuthentication no',
+  replace  => true,
 }
-
-file_line { 'Identiy file':
-    ensure   => present,
-    path     => '/etc/ssh/ssh_config',
-    line     => '   IdentyFile ~/.ssh/school',
-    replace  => true,
+file_line { 'Identity file':
+  ensure   => present,
+  path     => '/etc/ssh/ssh_config',
+  line     => 'IdentityFile ~/.ssh/school',
+  replace  => true,
 }
