a568bdcdb2905b8b80f592cd7fe59027d8f808bd
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-01-29 21:07:00 +0300

    feat: add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,19 @@
+# Adds a custom HTTP header
+exec { 'apt':
+    command => '/usr/bin/apt update'
+}
+
+package { 'nginx':
+    ensure => 'installed',
+    name   => 'nginx',
+}
+
+file_line { 'append a line in nginx config file':
+    path  => '/etc/nginx/nginx.conf',
+    line  => ;\tadd_header X-Served-By ${hostname};",
+    after => 'http {',
+}
+
+exec { 'sudo service nginx restart':
+    command => 'usr/sbin/service nginx restart',
+}
