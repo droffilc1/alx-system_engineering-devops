bd22e7722ac6758a1916eeae41df28397c8fdb7f
Clifford Mapesa <97587370+droffilc1@users.noreply.github.com>
2024-03-05 14:40:38 +0300

    feat: strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # Fixes Apache 500 error
+# https://stackoverflow.com/questions/174942/how-should-strace-be-used
 
 exec { 'fix-config':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
