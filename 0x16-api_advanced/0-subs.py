c2a09d07cc43fa146913d4f0d1e0fa27a487f4cd
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-03-05 10:19:02 +0300

    feat: how many subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Queries the Reddit API"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+        for a given subreddit
+    If an invalid subreddit is given, the function should return 0
+    """
+    # API endpoint for geeting subreddit information
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    # Set a custom User-Agent to avoid too many requests errors
+    headers = {'User-Agent': 'My User Agent 1.0'}
+
+    # Send a Get request
+    response = requests.get(url, headers=headers).json()
+    subscribers = response.get('data', {}).get('subscribers')
+
+    if not subscribers:
+        return 0
+    return subscribers
