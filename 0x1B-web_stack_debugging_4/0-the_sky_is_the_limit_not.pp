8ed8560095b64d71f97d6aa563f1e4f8d67cb8af
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-03-12 11:06:20 +0300

    feat: sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Increase Nginx open files limit
 
 exec { 'modify_nginx':
-	commnad => 'sed -i "s/15/4096/" /etc/default/nginx',
-	path    => '/usr/local/bin/:/bin/',
+    command => 'sed -i "s/15/4096/" /etc/default/nginx',
+    path    => '/usr/local/bin/:/bin/',
 }
 
 exec { 'nginx_restart':
-	command => 'sudo systemctl nginx restart',
-	path    => '/etc/init.d/',
+    command => 'nginx restart',
+    path    => '/etc/init.d/',
 }
